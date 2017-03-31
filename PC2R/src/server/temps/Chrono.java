2be214f7483f97090be8b9696962f3b97fe9b9ad
arthurescriou <arthur.escriou@gmail.com>
2017-03-31 10:58:21 +0200

    chronometre trop classe

--- PC2R/src/server/temps/Chrono.java
+++ PC2R/src/server/temps/Chrono.java
@@ -2,18 +2,35 @@ package server.temps;
 
 import java.util.Observable;
 
-public class Chrono extends Observable{
-	private double time;
-	
-	
-	public void start(){
-		time=System.currentTimeMillis();
-	}
-	
-	public int getTemps() {
-		return (int)((System.currentTimeMillis()-time)/1000);
+public class Chrono extends Observable {
+	private double	time;
+
+	private Thread t;
+	public void start() {
+		time = System.currentTimeMillis();
+		t=new Thread() {
+			public void run() {
+				while (true) {
+
+					try {
+						sleep(1000);
+					} catch (InterruptedException e) {
+						return;
+					}
+					setChanged();
+					notifyObservers();
+				}
+			};
+		};
+		t.start();
 	}
 
-	
+	public int getTemps() {
+		return (int) ((System.currentTimeMillis() - time) / 1000);
+	}
+
+	public void stop(){
+		t.interrupt();
+	}
 	
 }
